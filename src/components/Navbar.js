89146fd92d627c28584fc24860eacfab2db933c8
success ibekwe <Successibekwe047@gmail.com>
2022-11-11 02:48:03 +0100

    imported all the needed images

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav className="container mx-auto p-6">
+      <div className="flex justify-between items-center">
+        {/* logo */}
+        <div className="pt-2">
+            <img src={} alt="logo" />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
 
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {},
+    },
   },
   plugins: [],
 };
